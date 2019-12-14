ba383ba75cfc3d56ef48ff9cb2e09c345c9b229f
code4idea <36807315+code4idea@users.noreply.github.com>
2019-12-14 16:38:10 +0900

    Update Stack - list (Struct).c

--- Data Structure/Gyu/DataStructure/Stack - list (Struct).c
+++ Data Structure/Gyu/DataStructure/Stack - list (Struct).c
@@ -10,126 +10,56 @@
 #include <malloc.h>
 #define NULL 0
 
-using namespace std;
+template <typename T>
+struct Node
+{
+	Node<T>* next;
+	T data;
+};
 
-typedef struct node {
-	struct node* prev;
-	struct node* next;
-	int data;
-}Node;
+template <typename T>
+struct Stack
+{
+	Node<T>* topNode;
 
-typedef struct list {
-	Node* frontNode;
-	Node* backNode;
-	
-	void init();
-	void push_front(int value);
-	void push_back(int value);
-	int pop_front();
-	int pop_back();
-	Node* search_front(int value);
-	bool insert(Node* inputNode, int value);
-	bool remove(int value);
-	int front();
-	int back();
-	int empty();
-} List;
+	Stack();
 
-void List::init() {
-	frontNode = NULL;
-	backNode = NULL;
+	void push(T value);
+	T pop();
+	T top();
+	bool empty();
+};
+
+template <typename T>
+Stack<T>::Stack() {
+	topNode = NULL;
 }
 
-void List::push_front(int value) {
-	Node* newNode = (Node*)malloc(sizeof(Node));
-	newNode->prev = NULL;
-	newNode->next = frontNode;
+template <typename T>
+void Stack<T>::push(T value) {
+	Node<T>* newNode = (Node<T> *)malloc(sizeof(Node<T>));
+	newNode->next = topNode;
 	newNode->data = value;
+	topNode = newNode;
+};
 
-	frontNode->prev = newNode;
-	frontNode = newNode;
-	if (backNode == NULL) backNode = newNode;
-}
-
-void List::push_back(int value) {
-	Node* newNode = (Node*)malloc(sizeof(Node));
-	newNode->prev = backNode;
-	newNode->next = NULL;
-	newNode->data = value;
-
-	if(backNode != NULL) backNode->next = newNode;
-	backNode = newNode;
-	if (frontNode == NULL) frontNode = newNode;
-}
-
-int List::pop_front() {
+template <typename T>
+T Stack<T>::pop() {
 	if (empty()) return 0;
 
-	Node* delNode = frontNode;
-	int ret = frontNode->data;
-	frontNode = frontNode->next;
-	if (frontNode != NULL) frontNode->prev = NULL;
+	Node<T> * delNode = topNode;
+	T ret = topNode->data;
+	topNode = topNode->next;
 	free(delNode);
 	return ret;
 }
 
-int List::pop_back() {
-	if (empty()) return 0;
-
-	Node* delNode = backNode;
-	int ret = backNode->data;
-	backNode = backNode->prev;
-	if (backNode != NULL) backNode->next = NULL;
-	free(delNode);
-	return ret;
+template <typename T>
+T Stack<T>::top() {
+	return empty() ? 0 : topNode->data;
 }
 
-Node* List::search_front(int value) {
-	Node* ret = frontNode;
-	while (ret != NULL) {
-		if (ret->data == value) break;
-		ret = ret->next;
-	}
-	return ret;
-}
-
-bool List::insert(Node* inputNode, int value) {
-	if (inputNode == NULL) return false;
-	else {
-		Node* newNode = (Node*)malloc(sizeof(Node));
-		newNode->next = inputNode;
-		newNode->prev = inputNode->prev;
-		newNode->data = value;
-
-		inputNode->prev = newNode;
-
-		return true;
-	}
-}
-
-bool List::remove(int value) {
-	Node* searchNode = search_front(value);
-	if (searchNode == NULL) return false;
-	else {
-		while (searchNode != NULL) {
-			searchNode->prev->next = searchNode->next;
-			searchNode->next->prev = searchNode->prev;
-			
-			free(searchNode);
-
-			searchNode = search_front(value);
-		}
-	}
-}
-
-int List::front() {
-	return frontNode == NULL ? 0 : frontNode->data;
-}
-
-int List::back() {
-	return backNode == NULL ? 0 : backNode->data;
-}
-
-int List::empty() {
-	return (frontNode == backNode && frontNode == NULL) ? true : false;
+template <typename T>
+bool Stack<T>::empty() {
+	return topNode == NULL ? true : false;
 }
