854be45d02c106d9399af56954d08fa1a104bda4
Park Sang Hoon <21328080+ParkSang@users.noreply.github.com>
2019-12-21 18:19:03 +0900

    Update Boyer-Moore.cpp

--- Data Structure/Sang/String Search/Boyer-Moore.cpp
+++ Data Structure/Sang/String Search/Boyer-Moore.cpp
@@ -1,36 +1,33 @@
-#include <stdio.h>
-#include <string.h>
+#include <iostream>
 
-char *search(char *, char *);
-void table(char *);
+using namespace std;
 
 int skip[256];
+int len;
 
-int main(void)
+int length(char* c)
 {
-	static char text[] = "Using the Hecomdmentsllo World guide, you'll stacommentsrt a braHellonch, write comments, and open a pHellcommentsoull request.";
-	char *p;
-	char key[] = "comments";
-
-	table(key);
-	p = search(text, key);
-	while (p != NULL) {
-		printf("%s\n", p);
-		p = search(p + strlen(key), key);
+	for(int i=0;;i++)
+	{
+		char tmp = *(c+i);
+		if(tmp == 0)
+		{
+			return i;
+		}
 	}
-
-	return 0;
 }
 
-void table(char *key)    /* create skip table */
+void table(char* c)
 {
-	int k, n;
-
-	n = strlen(key);
-	for (k = 0; k <= 255; k++)
-		skip[k] = n;
-	for (k = 0; k < n - 1; k++)
-		skip[key[k]] = n - 1 - k;
+	len = length(c);
+	for(int i = 0;i<256;i++)
+	{
+		skip[i] = len;
+	}
+	for(int i = 0;i<len-1;i++)
+	{
+		skip[c[i]] = len - 1 - i;
+	}
 }
 
 char *search(char *text, char *key)
@@ -38,10 +35,11 @@ char *search(char *text, char *key)
 	int m, n;
 	char *p;
 
-	m = strlen(text);
-	n = strlen(key);
+	m = length(text);
+	n = length(key);
 
 	p = text + n - 1;
+
 	while (p < text + m) {
 			bool check = true;
 			for (int i = n - 1; i >= 0; i--)
@@ -58,4 +56,20 @@ char *search(char *text, char *key)
 	}
 
 	return NULL;
+}
+
+int main(void)
+{
+	static char text[] = "Using the Hecomdmentsllo World guide, you'll stacommentsrt a braHellonch, write comments, and open a pHellcommentsoull request.";
+	char *p;
+	char key[] = "comments";
+
+	table(key);
+	p = search(text, key);
+	while (p != NULL) {
+		printf("%s\n", p);
+		p = search(p + strlen(key), key);
+	}
+
+	return 0;
 }
